b98b3ed96360afabd37c63fa6a8bc18ebbab1298
Mattrix01 <emjay.knight@gmail.com>
2022-07-05 20:45:46 +0100

    Employee test started

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,23 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  it("That get name is being saved properly", () => {
+    const employeeNew = new Employee("employee name", 3, "email@dummy.com");
+    expect(employeeNew.getName()).toEqual("employee name");
+  });
+  it("That id is getting a number", () => {
+    const employeeNew = new Employee("employee name", 3, "email@dummy.com");
+    expect(employeeNew.getId()).toEqual(3);
+  });
+  it("That email is getting an email", () => {
+    const employeeNew = new Employee("employee name", 3, "email@dummy.com");
+    expect(employeeNew.getEmail()).toEqual("email@dummy.com");
+  });
+  it("will return the role of employee ", () => {
+    const employeeNew = new Employee("employee name", 3, "email@dummy.com");
+    expect(employeeNew.getRole()).toEqual("Employee");
+  });
+});
 // can i create an instance of a class - should be of type object
 
 // can i set the name on the employee class using constructor
